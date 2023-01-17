427798ff8fb508151ef1855db3b13c677595a4fa
s21016 <s21016@std.it-college.ac.jp>
2023-01-17 13:40:58 +0900

    データベースを取得するための処理を記述

--- app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/MainActivity.kt
@@ -2,10 +2,23 @@ package jp.ac.it_college.std.s21016.themedresearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import jp.ac.it_college.std.s21016.themedresearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //ヘルパーを準備
+        val helper = SimpleDatabaseHelper(this)
+
+        //データベースを取得
+        helper.writableDatabase.use { db ->
+            Toast.makeText(this, "接続しました",
+            Toast.LENGTH_SHORT).show()
+        }
     }
 }
