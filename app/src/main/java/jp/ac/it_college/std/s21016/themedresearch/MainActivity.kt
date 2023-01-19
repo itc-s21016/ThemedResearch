65f3a00ce61950bcd1bdfa590d144f70da7df38b
s21016 <s21016@std.it-college.ac.jp>
2023-01-19 10:50:10 +0900

    Sampleフォルダの削除

--- app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/MainActivity.kt
@@ -3,7 +3,7 @@ package jp.ac.it_college.std.s21016.themedresearch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import jp.ac.it_college.std.s21016.roomwordsample.databinding.ActivityMainBinding
+import jp.ac.it_college.std.s21016.themedresearch.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
