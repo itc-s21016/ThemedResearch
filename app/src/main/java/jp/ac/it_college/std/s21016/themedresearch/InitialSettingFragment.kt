58c96cd8537f2b56c168cf8d435c064b3f5c3291
itc-s21012 <s21012@std.it-college.ac.jp>
2023-01-31 09:33:12 +0900

    初期設定画面

--- app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/InitialSettingFragment.kt
+++ app/src/main/java/jp/ac/it_college/std/s21016/themedresearch/InitialSettingFragment.kt
@@ -1,7 +1,30 @@
 package jp.ac.it_college.std.s21016.themedresearch
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import jp.ac.it_college.std.s21016.themedresearch.databinding.FragmentInitialSettingBinding
 
 class InitialSettingFragment: Fragment() {
+    private var _binding: FragmentInitialSettingBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentInitialSettingBinding.inflate(inflater, container, false)
+
+        binding.btNext.setOnClickListener {
+            Navigation.findNavController(it).navigate(
+                InitialSettingFragmentDirections.actionInitialSettingFragmentToMainFragment()
+            )
+        }
+        return binding.root
+
+    }
 }
